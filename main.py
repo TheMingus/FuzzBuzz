4314a308f349fedc4579b9b5acdf58bf500d195d
TheMingus <90332127+TheMingus@users.noreply.github.com>
2022-03-01 13:08:13 -0600

    oiwuy yaw3iueyrofwqgefvaldhvbawegbgvf

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ while opt not in ['1','2']:
 
 to = input('Generate to what? ')
   
-if opt = '0':
+if opt == '0':
   start = time()
   final = ['Fuzz Buzz' if i%15==0 else 'Buzz' if i%3==0 else 'Fuzz' if i%5==0 else str(i) for i in tqdm(range(int(to)))]
   duration = time() - start
   print(f'Done! Took {duration} seconds. Don\'t stop yet, we are still writing the results to a file.')
   with open('final.json', 'w') as f: dump(final, f, indent=2)
   print('Ok, now we\'re done.')
-if opt = '1':
+if opt == '1':
   start = time()
   for i in tqdm(range(int(to))):
     if i%15==0:
